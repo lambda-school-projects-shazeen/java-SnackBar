7f90969882fb80a9c0433051bec9d80d4cfee619
Shazeen Fabius <shazeenfabius@Shazeens-MBP.lan>
2021-03-08 18:26:23 -0800

    set up vending maching and customer classes

---
+++ src/snackbar/VendingMachine.java
@@ -0,0 +1,44 @@
+package snackbar;
+
+public class VendingMachine
+{
+	/*
+Vending machine has fields (so knows)
+
+maxId - keep track of last used vending machine id
+id - automatically generated field
+name
+Vending Machine has methods (so can)
+
+get id
+set and get name
+	*/
+
+private static int maxId = 0;
+private int id;
+private String name;
+
+public VendingMachine(int id, String name)
+{
+	maxId++;
+	id = maxId;
+
+	this.id = id;
+	this.name = name;
+}
+
+public int getId()
+{
+	return id;
+}
+
+public String getName()
+{
+	return name;
+}
+
+public void setName(String name)
+{
+	this.name = name;
+}
+}
